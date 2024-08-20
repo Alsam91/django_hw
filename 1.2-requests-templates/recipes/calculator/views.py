7a6145fcd96ddde63ba1f600cdeb6ca2e84bcf94
Alexander <alsam91@gmail.com>
2024-08-20 23:09:11 +0300

    Second HW

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 DATA = {
     'omlet': {
         'яйца, шт': 2,
@@ -19,6 +20,7 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
@@ -28,3 +30,31 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+def omlet(request):
+    servings = request.GET.get('servings', 1)
+    for key, value in DATA['omlet'].items():
+        DATA['omlet'][key] *= int(servings)
+    context = {
+        'recipe': DATA['omlet']
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta(request):
+    servings = request.GET.get('servings', 1)
+    for key, value in DATA['pasta'].items():
+        DATA['pasta'][key] *= int(servings)
+    context = {
+        'recipe': DATA['pasta']
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def buter(request):
+    servings = request.GET.get('servings', 1)
+    for key, value in DATA['buter'].items():
+        DATA['buter'][key] *= int(servings)
+    context = {
+        'recipe': DATA['buter']
+    }
+    return render(request, 'calculator/index.html', context)
